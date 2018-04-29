03f47726e98a14a6b932a872f793a082f128aae3
Alberto <foliolo_@hotmail.com>
2018-04-29 02:52:08 +0200

    Initial commit
     * ADD: Comit inicial con el proyecto en blanco

---
+++ DeltaCsvParser/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeltaCsvParser {
+
+    /// <summary>
+    /// Inicio al programa principal
+    /// </summary>
+    class Program {
+
+        /// <summary>
+        /// Main Class
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args) {
+            adf;
+
+        }
+    }
+}
